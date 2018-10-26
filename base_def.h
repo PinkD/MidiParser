1d77f3bb5608cf619a2874eacd63a44830c47132
PinkD <443657547@qq.com>
2018-10-26 13:10:03 +0800

    remove dependency `tiff`

--- base_def.h
+++ base_def.h
@@ -1,6 +1,13 @@
 #ifndef BASE_DEF_LIBRARY_H
 #define BASE_DEF_LIBRARY_H
-#include <tiff.h>
+
+#include <stdint.h>
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
 
 typedef unsigned char byte;
+
 #endif
